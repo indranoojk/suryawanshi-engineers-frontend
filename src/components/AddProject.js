959a64bdcc514e8b834d7a7a1577f18228445b6d
indranoojk <indranoojk@gmail.com>
2025-01-08 23:46:33 +0530

    seperate add image

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -6,15 +6,19 @@ import { baseUrl } from "../Urls";
 import imageCompression from 'browser-image-compression';
 
 const AddProject = () => {
+
+  const [projects, setProjects] = useState([]);
+
   const [projectDetails, setProjectDetails] = useState({
     title: "",
     description: "",
     content: "",
-    image: "",
+    imageUrl: "",
   });
 
   const [file, setFile] = useState(null);
-  const [imageUrl, setImageUrl] = useState("");
+  const [imageLink, setImageLink] = useState("");
+  // const [cloudinaryUrl, setCloudinaryUrl] = useState("");
   const [feedback, setFeedback] = useState("");  // New state for feedback messages
 
 
@@ -27,11 +31,18 @@ const AddProject = () => {
     return await imageCompression(file, options);
   };
 
+  // const handleChange = async (e) => {
+  //   const selectedFile = e.target.files[0];
+  //   const compressedFile = await compressImage(selectedFile);
+  //   setFile(compressedFile);
+  //   previewFiles(compressedFile);
+  // };
+
   const handleChange = async (e) => {
     const selectedFile = e.target.files[0];
-    const compressedFile = await compressImage(selectedFile);
-    setFile(compressedFile);
-    previewFiles(compressedFile);
+    // const compressedFile = await compressImage(selectedFile);
+    setFile(selectedFile);
+    previewFiles(selectedFile);
   };
 
   function previewFiles(file) {
@@ -39,64 +50,82 @@ const AddProject = () => {
     reader.readAsDataURL(file);
 
     reader.onloadend = () => {
-      setImageUrl(reader.result);
+      setImageLink(reader.result);
       console.log(reader.result); // Log the data URL after it's set
     }
     // console.log(image);
   }
+
+  const handleAddImage = async (e) => {
+    e.preventDefault();  // Prevent form submission from reloading the page
+
+    try {
+
+      let response = await fetch(`${baseUrl}/api/image/upload`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ imageLink }), // Send image in the body
+      });
+
+      const imageObj = await response.json();
+      console.log(imageObj);
+      setFeedback(imageObj.success ? alert("Image link generated successfully!") : alert("Failed to generate image link"));
+    } catch (error) {
+      setFeedback(alert(`Failed to upload image because ${error}`));
+      console.log(error);
+    }
+
+    setFile(null);
+
+  };
+
+  // const changeImageHandler = (e) => {
+  //   setImageDetails({ ...imageDetails, [e.target.name]: e.target.value });
+  // };
 
   const handleAddProject = async (e) => {
     e.preventDefault();  // Prevent form submission from reloading the page
 
     try {
       let project = projectDetails;
-      
-      let response = await fetch(`${baseUrl}/api/project/images/upload`, {
-        method: 'POST',
-        body: JSON.stringify({ imageUrl }), // Send image in the body
+      const addProjectResponse = await fetch(`${baseUrl}/api/project/addproject`, {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
+        body: JSON.stringify({ project }),
       });
 
-      let dataObj = await response.json();
-
-      if (dataObj.success) {
-        project.image = dataObj.image_url;
-        let addProjectResponse = await fetch(`${baseUrl}/api/project/addproject`, {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ project }),
-        });
-
-        let addProjectData = await addProjectResponse.json();
-        console.log(addProjectData);
-        setFeedback(addProjectData.success ? alert("Project added successfully!") : alert("Failed to add project"));
-        // setFeedback(addProjectData.success ? Swal.fire({
-        //   title: 'Success!',
-        //   text: 'Project added successfully!',
-        //   icon: 'success',
-        //   confirmButtonText: 'OK'
-        // }) : Swal.fire({
-        //   title: 'Error!',
-        //   text: 'Failed to add project',
-        //   icon: 'error',
-        //   confirmButtonText: 'OK'
-        // }));
-      }
+      const addProjectData = await addProjectResponse.json();
+      setProjects(projects.concat(addProjectData));
+      console.log(addProjectData);
+      setFeedback(addProjectData.success ? alert("Project added successfully!") : alert("Failed to add project"));
+      // setFeedback(addProjectData.success ? Swal.fire({
+      //   title: 'Success!',
+      //   text: 'Project added successfully!',
+      //   icon: 'success',
+      //   confirmButtonText: 'OK'
+      // }) : Swal.fire({
+      //   title: 'Error!',
+      //   text: 'Failed to add project',
+      //   icon: 'error',
+      //   confirmButtonText: 'OK'
+      // }));
     } catch (error) {
       setFeedback(alert(`Failed to upload project because ${error}`));
       console.log(error);
     }
 
-    setProjectDetails({ title: "", description: "", content: "", image: "" })
+    setProjectDetails({ title: "", description: "", content: "", imageUrl: "" })
     setFile(null);
-    setImageUrl("");
+    // setImageUrl("");
 
   };
+
 
   const changeHandler = (e) => {
     setProjectDetails({ ...projectDetails, [e.target.name]: e.target.value });
@@ -109,6 +138,32 @@ const AddProject = () => {
       <div className="text-xl lg:text-3xl ml-10 lg:ml-0 mt-4 font-semibold italic font-serif text-center">
         Hello! Mr. Suryawanshi
       </div>
+      {/* Form for adding image */}
+      <form onSubmit={handleAddImage}>  {/* Attach event handler to form submission */}
+        <div className="p-5 mb-20 text-white lg:flex pb-10" style={{ backgroundImage: `url(${'https://images.pexels.com/photos/19453624/pexels-photo-19453624/free-photo-of-high-line.jpeg?auto=compress&cs=tinysrgb&w=1280'})`, objectFit: 'contain' }}  >
+          <h2 style={{ fontFamily: "'Cinzel', serif" }} className="text-4xl font-bold font-serif w-48 m-20 text-opacity-85">
+            ADD AN IMAGE
+          </h2>
+          <div className="m-4 lg:m-10 lg:ml-20 lg:grid lg:grid-cols-2">
+
+            <div className="mb-3 lg:m-2">
+              <p className="text-xs lg:text-base font-semibold text-[#dad1bf]">Select Image*</p>
+              <label htmlFor="image">
+                <img className="p-2 w-48 h-32 object-contain" src={!file ? upload_area : URL.createObjectURL(file)} alt="" />
+              </label>
+              <input onChange={handleChange} type="file" accept="image/*" id="image" name="image" hidden />
+            </div>
+            <div>
+              <button disabled={!file} type="submit" className="bg-green-500 disabled:bg-gray-500 disabled:text-gray-400 ml-20 lg:ml-20 px-12 lg:px-16 py-3 border-2 border-[#716c6a] shadow-sm hover:shadow-xl shadow-[#f5f2f2] focus:outline-none focus:shadow-outline-blue">
+                SUBMIT
+              </button>
+            </div>
+          </div>
+          {feedback && <div className="feedback-message">{feedback}</div>}  {/* Render feedback message */}
+        </div>
+      </form>
+
+      {/* Form for adding project */}
       <form onSubmit={handleAddProject}>  {/* Attach event handler to form submission */}
         <div className="p-5 text-white lg:flex pb-10" style={{ backgroundImage: `url(${'https://images.pexels.com/photos/19453624/pexels-photo-19453624/free-photo-of-high-line.jpeg?auto=compress&cs=tinysrgb&w=1280'})`, objectFit: 'contain' }}  >
           <h2 style={{ fontFamily: "'Cinzel', serif" }} className="text-4xl font-bold font-serif w-48 m-20 text-opacity-85">
@@ -129,14 +184,11 @@ const AddProject = () => {
               <textarea className="input-class mt-0 block w-full px-0.5 bg-transparent border-0 border-b-2 border-[#716c6a] focus:ring-0 focus:border-gray-400" name="content" id="content" value={projectDetails.content} onChange={changeHandler} cols="30" rows="5" required />
             </div>
             <div className="mb-3 lg:m-2 row-span-2">
-              <p className="text-xs lg:text-base font-semibold text-[#dad1bf]">Select Image*</p>
-              <label htmlFor="image">
-                <img className="p-2 w-48 h-32 object-contain" src={!file ? upload_area : URL.createObjectURL(file)} alt="" />
-              </label>
-              <input onChange={handleChange} type="file" accept="image/*" id="image" name="image" hidden />
+              <label htmlFor="imageUrl" className="text-xs lg:text-base font-semibold text-[#dad1bf]"> Image* </label>
+              <input type="text" className="input-class mt-0 block w-full lg:w-80 px-0.5 bg-transparent border-0 border-b-2 border-[#716c6a] focus:ring-0 focus:border-gray-400" id="imageUrl" name="imageUrl" value={projectDetails.imageUrl} onChange={changeHandler} required />
             </div>
             <div>
-              <button disabled={projectDetails.title.length < 3 || projectDetails.description.length < 5 || projectDetails.content.length < 15 || !imageUrl} type="submit" className="bg-green-500 disabled:bg-gray-500 disabled:text-gray-400 ml-20 lg:ml-20 px-12 lg:px-16 py-3 border-2 border-[#716c6a] shadow-sm hover:shadow-xl shadow-[#f5f2f2] focus:outline-none focus:shadow-outline-blue">
+              <button disabled={projectDetails.title.length < 3 || projectDetails.description.length < 5 || projectDetails.content.length < 15 || projectDetails.imageUrl.length < 5} type="submit" className="bg-green-500 disabled:bg-gray-500 disabled:text-gray-400 ml-20 lg:ml-20 px-12 lg:px-16 py-3 border-2 border-[#716c6a] shadow-sm hover:shadow-xl shadow-[#f5f2f2] focus:outline-none focus:shadow-outline-blue">
                 SUBMIT
               </button>
             </div>
